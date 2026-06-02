05730c95f86488ebaeeb470ff801a618b0b40ad7
Yuke Ma <yukema@inet-p4lab-gpu-01.mpi-inf.mpg.de>
2026-06-02 18:52:25 +0200

    feat: SSR application device driver framework
    1. host/kernel for application device driver
    2. fix some errors of FPGA

--- prototype/fpga/config.tcl
+++ prototype/fpga/config.tcl
@@ -120,14 +120,14 @@ dict set params AXI_DDR_ID_WIDTH "8"
 dict set params AXI_DDR_MAX_BURST_LEN "256"
 
 # Application block configuration
-dict set params APP_ID "32'h00000000"
-dict set params APP_ENABLE "0"
+dict set params APP_ID "32'h53535201"
+dict set params APP_ENABLE "1"
 dict set params APP_CTRL_ENABLE "1"
-dict set params APP_DMA_ENABLE "1"
-dict set params APP_AXIS_DIRECT_ENABLE "1"
-dict set params APP_AXIS_SYNC_ENABLE "1"
-dict set params APP_AXIS_IF_ENABLE "1"
-dict set params APP_STAT_ENABLE "1"
+dict set params APP_DMA_ENABLE "0"
+dict set params APP_AXIS_DIRECT_ENABLE "0"
+dict set params APP_AXIS_SYNC_ENABLE "0"
+dict set params APP_AXIS_IF_ENABLE "0"
+dict set params APP_STAT_ENABLE "0"
 
 # DMA interface configuration
 dict set params DMA_IMM_ENABLE "0"
